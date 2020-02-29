57ea8775aab680a389283a0e077f59daadaab0b5
queyzh <qyz12321@163.com>
2020-02-29 22:44:45 +0800

    删除其它不用的，准备改造

--- SpringBoot-demo/demo02/src/main/java/com/vi/service/UserInfoInterface.java
+++
@@ -1,7 +0,0 @@
-package com.vi.service;
-
-import com.vi.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoInterface extends JpaRepository<UserInfo, Long> {
-}
